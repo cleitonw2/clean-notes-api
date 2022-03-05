932b20d23248880be30fb51b7cac347c4a793ee5
Cleiton <cleitonwoycik@outlook.com>
2022-03-05 16:29:23 -0300

    feat: ensure SignUpController returns 500 if Valiation throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,13 +1,17 @@
-import { badRequest } from '../helpers'
+import { badRequest, serverError } from '../helpers'
 import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class SignUpController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (request: SignUpController.Request): Promise<HttpResponse> {
-    const error = this.validation.isValid(request) as any
-    if (error) return badRequest(error)
-    return null as any
+    try {
+      const error = this.validation.isValid(request) as any
+      if (error) return badRequest(error)
+      return null as any
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
