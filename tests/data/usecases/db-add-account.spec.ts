929752c4ab41e6d16dd74d6f6dd45d3056c02a42
Cleiton <cleitonwoycik@outlook.com>
2022-03-05 11:04:15 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- tests/data/usecases/db-add-account.spec.ts
+++ tests/data/usecases/db-add-account.spec.ts
@@ -30,6 +30,18 @@ describe('DbAddAccount', () => {
     expect(params).toEqual(addAccountRepositorySpy.params)
   })
 
+  it('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositorySpy } = makeSut()
+    jest.spyOn(addAccountRepositorySpy, 'add').mockRejectedValueOnce(new Error())
+    const params = {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
+    const promise = sut.add(params)
+    expect(promise).rejects.toThrow()
+  })
+
   it('Should call Hasher with correct value', async () => {
     const { sut, hasherSpy } = makeSut()
     const params = {
