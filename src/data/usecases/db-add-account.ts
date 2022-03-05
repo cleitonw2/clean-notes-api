a62bf9c1fab684b4971bec10fd0db06d0f98783b
Cleiton <cleitonwoycik@outlook.com>
2022-03-05 10:57:10 -0300

    feat: ensure calls Hasher with correct value

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,10 +1,14 @@
 import { AddAccount } from '@/usecases/add-account'
-import { AddAccountRepository } from '../protocols'
+import { AddAccountRepository, Hasher } from '../protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly hasher: Hasher
+  ) {}
 
   async add (data: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.hasher.hash(data.password)
     await this.addAccountRepository.add(data)
     return null as any
   }
