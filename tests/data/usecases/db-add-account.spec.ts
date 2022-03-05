4525dcb25ec115a67c32d5c5f4523ca6ca35b26d
Cleiton <cleitonwoycik@outlook.com>
2022-03-05 10:49:48 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/db-add-account.spec.ts
+++ tests/data/usecases/db-add-account.spec.ts
@@ -1,10 +1,23 @@
 import { DbAddAccount } from '@/data/usecases'
 import { AddAccountRepositorySpy } from '../mocks'
 
+type SutTypes = {
+  sut: DbAddAccount
+  addAccountRepositorySpy: AddAccountRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountRepositorySpy = new AddAccountRepositorySpy()
+  const sut = new DbAddAccount(addAccountRepositorySpy)
+  return {
+    sut,
+    addAccountRepositorySpy
+  }
+}
+
 describe('DbAddAccount', () => {
   it('Should call AddAccountRepository with correct values', async () => {
-    const addAccountRepositorySpy = new AddAccountRepositorySpy()
-    const sut = new DbAddAccount(addAccountRepositorySpy)
+    const { sut, addAccountRepositorySpy } = makeSut()
     const params = {
       name: 'any_name',
       email: 'any_email',
