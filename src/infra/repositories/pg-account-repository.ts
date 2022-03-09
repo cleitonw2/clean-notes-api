eb7c0c80f2e30c65626199eed8f5928eac07361f
Cleiton <cleitonwoycik@outlook.com>
2022-03-09 13:05:05 -0300

    feat: ensure PgAccountRepository returns false if account not found

--- src/infra/repositories/pg-account-repository.ts
+++ src/infra/repositories/pg-account-repository.ts
@@ -1,11 +1,19 @@
-import { AddAccountRepository } from '@/data/protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols'
 import { prismaClient } from '@/infra/helpers'
 
-export class PgAccountRepository implements AddAccountRepository {
+export class PgAccountRepository implements AddAccountRepository, CheckAccountByEmailRepository {
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     return await prismaClient.account.create({
       data,
       select: { id: true }
+    })
+  }
+
+  async checkByEmail (email: string): Promise<boolean> {
+    return !!await prismaClient.account.findUnique({
+      where: {
+        email
+      }
     })
   }
 }
