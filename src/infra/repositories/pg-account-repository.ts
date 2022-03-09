16658ac45403f5c8e48df40c1e02857080050432
Cleiton <cleitonwoycik@outlook.com>
2022-03-09 12:43:02 -0300

    feat: ensure PgAccountRepository returns id on success

--- src/infra/repositories/pg-account-repository.ts
+++ src/infra/repositories/pg-account-repository.ts
@@ -3,9 +3,9 @@ import { prismaClient } from '@/infra/helpers'
 
 export class PgAccountRepository implements AddAccountRepository {
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
-    await prismaClient.account.create({
-      data
+    return await prismaClient.account.create({
+      data,
+      select: { id: true }
     })
-    return null as any
   }
 }
