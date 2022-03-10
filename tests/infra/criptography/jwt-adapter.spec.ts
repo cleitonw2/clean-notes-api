7337aadc2f650cac279399d453ead14ad2c3e6dc
Cleiton <cleitonwoycik@outlook.com>
2022-03-10 14:48:52 -0300

    test: ensure JwtAdapter throws if sign throws

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -19,10 +19,19 @@ describe('jwt', () => {
       expect(hashSpy).toHaveBeenCalledWith({ id: 'any_value' }, secret)
     })
 
-    it('Should return a token on success', async () => {
+    it('Should return a token on sign success', async () => {
       const sut = makeSut()
       const token = await sut.encrypt('any_value')
       expect(token).toBe('any_token')
+    })
+
+    it('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.encrypt('any_value')
+      expect(promise).rejects.toThrow()
     })
   })
 })
