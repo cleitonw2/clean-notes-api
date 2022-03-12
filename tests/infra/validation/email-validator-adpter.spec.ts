f90bce31a9b9e4c7c776806333752c98d1e6442c
Cleiton <cleitonwoycik@outlook.com>
2022-03-12 16:23:42 -0300

    test: ensure EmailValidatorAdapter returns false if isEmail returns false

--- tests/infra/validation/email-validator-adpter.spec.ts
+++ tests/infra/validation/email-validator-adpter.spec.ts
@@ -7,11 +7,20 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => new EmailValidatorAdapter()
+
 describe('EmailValidatorAdapter', () => {
   it('Should call isEmail with correct value', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  it('Should return false if isEmail returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('any_email@mail.com')
+    expect(isValid).toBe(false)
   })
 })
