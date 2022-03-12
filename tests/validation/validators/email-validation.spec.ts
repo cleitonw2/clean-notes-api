fa08aacc9d56b33afbae38b6a8d1bd04bfe395e8
Cleiton <cleitonwoycik@outlook.com>
2022-03-12 14:53:19 -0300

    test: ensure EmailValidation returns error if EmailValidator returns false

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -1,5 +1,6 @@
 import { EmailValidation } from '@/validation/validators'
 import { EmailValidatorSpy } from '@/../tests/validation/mocks'
+import { InvalidParamError } from '@/presentation/errors'
 
 type SutTypes = {
   sut: EmailValidation
@@ -23,5 +24,12 @@ describe('EmailValidation', () => {
     const { sut, emailValidatorSpy, email } = makeSut()
     sut.isValid({ email: email })
     expect(emailValidatorSpy.email).toBe(email)
+  })
+
+  it('Should return a error if EmailValidator returns false', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.resutl = false
+    const error = sut.isValid({ email: 'any_email' })
+    expect(error).toEqual(new InvalidParamError('email'))
   })
 })
