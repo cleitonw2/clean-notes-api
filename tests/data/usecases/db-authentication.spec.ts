7656b09aa952b663e0120860c692c8752cc81b8a
Cleiton <cleitonwoycik@outlook.com>
2022-03-23 15:09:33 -0300

    test: ensure DbAuthentication returns false if LoadAccount repo returns null

--- tests/data/usecases/db-authentication.spec.ts
+++ tests/data/usecases/db-authentication.spec.ts
@@ -29,7 +29,13 @@ describe('DbAddAccount', () => {
     expect(authParams.email).toEqual(loadAccountByEmailRepositorySpy.email)
   })
 
-  it.todo('Should return false if LoadAccountByEmailRepository returns null')
+  it('Should return false if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositorySpy } = makeSut()
+    loadAccountByEmailRepositorySpy.result = null as any
+    const result = await sut.auth(authParams)
+    expect(result).toBe(false)
+  })
+
   it.todo('Should throw if LoadAccountByEmailRepository throws')
 
   it.todo('Should call HashComparer with correct values')
