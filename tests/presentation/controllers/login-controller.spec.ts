5a518b0e2be9d99862cf22510a28f7213ce1c14b
Cleiton <cleitonwoycik@outlook.com>
2022-03-28 11:30:41 -0300

    test: ensure LoginController calls Validation with correct values

---
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -0,0 +1,42 @@
+import { LoginController } from '@/presentation/controllers'
+import { ValidationSpy } from '../mocks'
+
+const mockRequest = (): LoginController.Request => ({
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+type SutTypes = {
+  sut: LoginController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new LoginController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+describe('Login Controller', () => {
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(validationSpy.data).toEqual(request)
+  })
+
+  it.todo('Should return 400 if Validation returns error')
+
+  it.todo('Should return 500 if Validation throws')
+
+  it.todo('Should call Authentication with correct values')
+
+  it.todo('Should return 403 if Authentication returns false')
+
+  it.todo('Should return 500 if Authentication throws')
+
+  it.todo('Should return a accessToken on success')
+})
